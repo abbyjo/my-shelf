b03d2c99e3eaab2aae7931c6a03209e2309cf41e
abbyjo <abbyjo225@gmail.com>
2023-12-06 22:23:22 -0500

    Added Login component formatting

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,13 +1,19 @@
 const Login = () => {
 
     return (
-      <main>
-
-
-
-
-
-      </main>
+      <section className="container mx-4">
+        <form className="m-5 align-items-center">
+          <div className="mb-3 w-75">
+            <label for="email" className="form-label">Username</label>
+            <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+          </div>
+          <div className="mb-3 w-75">
+            <label for="password" className="form-label">Password</label>
+            <input type="password" className="form-control" id="password" />
+          </div>
+          <button type="submit" className="btn btn-warning">Enter</button>
+        </form>
+      </section>
     );
   };
   
